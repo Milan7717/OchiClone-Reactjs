a21ff4bb0b3308b5ce3614a6e542423a92c36a31
milan <milan1mishra2@gmail.com>
2024-02-16 22:50:48 +0545

    LandingPage and MArquee page made

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,18 +3,24 @@ import { FaArrowUpLong } from "react-icons/fa6";
 const LandingPage = () => {
   return (
     <div className="w-full h-screen bg-zinc-900 pt-1 ">
-      <div className="textstructure mt-32 px-20">
-        {["We Create", "Eye Opening", "Presentations"].map((items, index) => {
+      <div className="textstructure mt-m2 px-20">
+        {["We Create", "Eye Opening", " Presentations"].map((item, index) => {
           return (
-            <div key={index} className="masker">
-              <h1 className="uppercase text-7xl leading-[4.5vw] tracking-tighter font-['Fponders_Grotesk']  font-semibold">
-                {items}
-              </h1>
+            <div className="masker">
+              <div className="w-fit flex items-end overflow-hidden">
+                {index === 1 && (
+                  <div className="mr-[1vw] w-[7vw] rounded-md h-[4.5vw] relative bg-green-500"></div>
+                )}
+                <h1 className="pt-[2vw] -mb-[1vw] uppercase text-[6vw] leading-[6vw] font-['Founders_Grotesk_X-Condensed'] ">
+                  {item}
+                </h1>
+              </div>
             </div>
           );
         })}
       </div>
-      <div className="border-t-[1px] border-zinc-800 mt-32 flex justify-between items-center py-5 px-20">
+
+      <div className="border-t-[1px] border-zinc-800 mt-20 flex justify-between items-center py-5 px-20">
         {[
           "For public and private companies",
           "From the first pitch to IPO",
@@ -31,7 +37,9 @@ const LandingPage = () => {
             start the project
           </div>
           <div className="w-10 h-10 flex items-center justify-center  border-[1px] border-zinc-500  rounded-full">
-          <span className="rotate-[45deg]" ><FaArrowUpLong /></span>
+            <span className="rotate-[45deg]">
+              <FaArrowUpLong />
+            </span>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import LandingPage from "../components/LandingPage";
+import Marquee from "../components/Marquee";
+import About from "../components/About";
+import Eyes from "../components/Eyes";
 
 const Home = () => {
   return (
-    <div className="w-full h-screen text-white">
+    <div className="w-full min-h-screen text-white bg-zinc-900">
       <Navbar />
 
       <LandingPage />
+      <Marquee />
+      <About />
+      <Eyes />
     </div>
   );
 };
