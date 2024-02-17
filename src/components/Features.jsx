e3c36fa51e47b503aa1fea8305edf0566fe7218d
milan <milan1mishra2@gmail.com>
2024-02-17 22:54:30 +0545

    Features part made

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -8,9 +8,35 @@ const Features = () => {
           Featured Projects
         </h1>
       </div>
-      <div className="cards w-full flex gap-10">
-        <div className="card w-1/2 h-[60vh] bg-red-500"></div>
-        <div className="card w-1/2 h-[60vh] bg-red-500"></div>
+      <div className="px-20">
+        <div className="cards w-full flex gap-10 mt-10">
+          <div className="cardcontainer relative w-1/2 h-[60vh]">
+          <h1 className="absolute left-full text-[#CDEA68] -translate-x-1/2 top-1/2 -translate-y-1/2 uppercase z-[9] text-6xl leading-none font-['Founders_Grotesk_X-Condensed']">
+              {"FYDE".split("").map((item, index) => (
+                <span className="">{item}</span>
+              ))}
+            </h1>
+            <div className=" card scale-50 rounded-xl bg-zinc-300 overflow-hidden w-full h-full">
+              <img
+                src="https://th.bing.com/th/id/OIG.MxQxUggA0RKmKdTjwAqw"
+                className="w-full h-full bg-cover object-center"
+              />
+            </div>
+          </div>
+          <div className="cardcontainer relative w-1/2 h-[60vh]">
+            <h1 className="absolute right-full text-[#CDEA68] translate-x-1/2 top-1/2 -translate-y-1/2 uppercase z-[9] text-6xl leading-none font-['Founders_Grotesk_X-Condensed']">
+              {"VISE".split("").map((item, index) => (
+                <span className="">{item}</span>
+              ))}
+            </h1>
+            <div className=" card scale-50 rounded-xl bg-zinc-300 overflow-hidden w-full h-full">
+              <img
+                src="https://th.bing.com/th/id/OIG.MxQxUggA0RKmKdTjwAqw"
+                className="w-full h-full bg-cover object-center"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
